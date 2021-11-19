dee6896c95b542b14d42e6ca19a4db08a6a01b2f
James OConnor <jamesedoconnor@gmail.com>
2021-11-19 02:03:47 +0000

    Added Distributed Ledger Technology section to paper

--- project/server/app.py
+++ project/server/app.py
@@ -5,15 +5,27 @@ import itertools, sys
 import subprocess
 import re
 
-# client = iota_client.Client(nodes_name_password=[['https://api.lb-0.h.chrysalis-devnet.iota.cafe/']])
+
+# Create the client using the IOTA client libs for Python 
 client = iota_client.Client(nodes_name_password=[['api.lb-0.testnet.chrysalis2.com']])
+
+# Set the topic for wexford charging stations 
+#################
+### TO DO ######
+################
+# Add dynamic finding using the vehicles location 
+
 topic = "charging-stations/wexford/"
 
 
 pushTopic = 'messages/indexation/' + topic
+
+# If the car drops to this level, then we are looking for a charge
 battery_threshold = 40
 
 
+
+# This only works on Mac (we are checking the battery level of the Mac PC) 
 def check_battery_level():
     battery_level_command = ['pmset', '-g', 'batt']
     battery_level_string = subprocess.run(battery_level_command, capture_output=True)
@@ -23,11 +35,13 @@ def check_battery_level():
     return battery_level
 
 
+# Add dynamic finding here
 def get_current_location():
     location = "wexford"
     return location
 
 
+# Each response 
 def send_result(result):
     result = str(result).encode("utf8")
     message = client.message(
@@ -44,11 +58,11 @@ def __spinner__():
         sys.stdout.write('\b')
 
 
-# Process Ticket Data
+# Process Charge Point Data
 def process_data(data):
-    print("data received")
+    print("Data received from car")
     payload_str = json.loads(data)["payload"]
-    # print("payload_str:", payload_str)
+
     message_id = client.get_message_id(payload_str)
     print("message_id:", message_id)
     message = client.get_message_data(message_id)
@@ -57,7 +71,7 @@ def process_data(data):
     message_data = bytes(message_bytes).decode("utf-8")
     print("message_data:", message_data)
 
-    send_result("Accept")
+    # We only accept if the battery level is low enough (force this if laptop is pluggged in)
     battery_level = check_battery_level()
     if int(battery_level) < battery_threshold:
         print(f"Battery level ({battery_level}) below threshold ({battery_threshold}). Charge Required!")
@@ -66,6 +80,12 @@ def process_data(data):
 
 # Receive MQTT msg
 def __main__():
+
+    # The client here is the IOTA client connected to the testnet network
+    # The push topic is a combo off the indexation endpoint combined with the user defined index 
+    # The subscribe_topic function is an asynchronous function that awaits the response via the callback function provided 
+    # 
+
     client.subscribe_topic(pushTopic, process_data)
     __spinner__()
 
